f10874c96b007a13aaec8124e435042159807f10
TudorPopescu24 <popescutudor66@gmail.com>
2022-04-13 18:22:53 +0300

    Navbar v3

---
+++ Scripts/navbar.js
@@ -0,0 +1,62 @@
+var clickedNotifications = false;
+var clickedProfile = false;
+
+// User clickedNotifications on notifications button
+function handleDropdownClick() {
+  const dropdown = document.getElementById("myDropdown");
+  dropdown.classList.toggle("show");
+  const dropdownButton = document.getElementById("dropdownButton");
+  if (!clickedNotifications) {
+    dropdownButton.style.backgroundColor = "#D3D3D3";
+    clickedNotifications = true;
+  } else {
+    dropdownButton.style.backgroundColor = "transparent";
+    clickedNotifications = false;
+  }
+}
+
+window.onclick = function (event) {
+  // Close the notifications dropdown menu if the user clicks outside of it
+  if (!(event.target.id == "dropdownButtonIcon")) {
+    const dropdownButton = document.getElementById("dropdownButton");
+    const dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains("show")) {
+        openDropdown.classList.remove("show");
+        dropdownButton.style.backgroundColor = "transparent";
+        clickedNotifications = false;
+      }
+    }
+  }
+
+  // Close the profile dropdown menu if the user clicks outside of it
+  if (!(event.target.id == "profilePic")) {
+    const dropdowns = document.getElementsByClassName("dropdown-content-ppic");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains("show")) {
+        openDropdown.classList.remove("show");
+        const profilePicArrow = document.getElementById("profilePicArrow");
+        profilePicArrow.className = "fa-solid fa-caret-down";
+        clickedProfile = false;
+      }
+    }
+  }
+
+  // User clicked on profile pic
+  if (event.target.id == "profilePic" || event.target.id == "profilePicArrow") {
+    const dropdown = document.getElementById("myDropdownPpic");
+    dropdown.classList.toggle("show");
+    const profilePicArrow = document.getElementById("profilePicArrow");
+    if (clickedProfile == false) {
+      profilePicArrow.className = "fa-solid fa-caret-up";
+      clickedProfile = true;
+    } else {
+      profilePicArrow.className = "fa-solid fa-caret-down";
+      clickedProfile = false;
+    }
+  }
+};

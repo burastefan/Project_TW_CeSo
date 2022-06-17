f7a6a9adc4bde9e1a5d1ac5e209af8a8a498ce96
Bura Stefan <stefan.bura@amplified.software>
2022-06-17 16:59:13 +0300

    finished to integrate Register Page + Send Email

--- BackEnd-CoSe/Controllers/authenticationController.js
+++ BackEnd-CoSe/Controllers/authenticationController.js
@@ -6,24 +6,39 @@
 const authenticationUser = require("../Models/authenticationModel");
 const { jsonType } = require("../Utils/headerTypes");
 const bcrypt = require("bcrypt");
+const { reject } = require("bcrypt/promises");
 
-const hashPassword = (password) => { 
-    return bcrypt.hashSync(password, 10);
-  };
+const hashPassword = (password) => {
+  return bcrypt.hashSync(password, 10);
+};
 
 async function registerUser(user, req, res) {
   try {
     user.password = hashPassword(user.password);
-    
-    await authenticationUser.addUser(user);
+    const userExist = await authenticationUser.checkEmail(user.email);
+    const userRegister = await authenticationUser.addUser(user);
+    console.log('userRegister:', userRegister);
+    console.log('userRegister.email:', userRegister.email);
 
-    res.writeHead(201, jsonType);
-    res.end(JSON.stringify({ message: "User registered succesfully!" }));
-
-
+    if (userExist != 0) {
+      res.writeHead(409, jsonType);
+      res.end(
+        JSON.stringify({ message: "This email address is already being used!" })
+      );
+    } else {
+      if (userRegister) {
+        await authenticationUser.generateCode(userRegister.email);
+        console.log("Send Email generated code!");
+      } else {
+        reject("Cannot add User.");
+      }
+      res.writeHead(201, jsonType);
+      res.end(
+        JSON.stringify({ message: "You have been successfully registered!" })
+      );
+    }
   } catch (error) {
     console.log("Error: ", error);
-
     res.writeHead(400, jsonType);
     res.end(JSON.stringify({ message: "Error in Registration pass!" }));
   }
--- BackEnd-CoSe/Models/authenticationModel.js
+++ BackEnd-CoSe/Models/authenticationModel.js
@@ -2,9 +2,161 @@ const dbContext = require("../Utils/dbContext");
 const { Request } = require("tedious");
 const TYPES = require("tedious").TYPES;
 const nodemailer = require("nodemailer");
+const randomString = require("randomstring");
 
+function getUserId(email) {
+  return new Promise((resolve, reject) => {
+    const connection = dbContext.connect();
 
-function addUser(event) {
+    connection.on("connect", (err) => {
+      if (err) {
+        reject(err.message);
+      } else {
+        const request = new Request(
+          `SELECT id FROM Users WHERE email = @email`,
+          (error) => {
+            if (error) {
+              reject(error.message);
+            }
+          }
+        );
+
+        request.addParameter("email", TYPES.VarChar, email);
+
+        try {
+          connection.execSql(request);
+
+          request.on("row", (rows) => {
+            rows.forEach((row) => {
+              response = row.value;
+              resolve(response);
+            });
+          });
+        } catch (error) {
+          reject(error);
+        }
+      }
+    });
+    connection.connect();
+  });
+}
+
+async function sendMail(mail, code) {
+  return new Promise(function (resolve, reject) {
+    var transporter = nodemailer.createTransport({
+      host: 'smtp.ethereal.email',
+      port: 587, 
+      auth: {
+        user: "deanna.grimes49@ethereal.email",
+        pass: "8ZJvNqh5zdSTHgX3GR",
+      },
+    });
+    var mailOptions = {
+      from: "cose.admin@gmail.com",
+      to: mail,
+      subject: "Validation code",
+      text: "Use this code: " + code + " to validate your account!",
+    };
+    transporter.sendMail(mailOptions, function (error, info) {
+      if (error) {
+        console.log(error);
+        reject(false);
+      } else {
+        resolve(true);
+        console.log("Email sent: " + info.response);
+      }
+    });
+  });
+}
+
+async function generateCode(email) {
+  const userId = await getUserId(email);
+
+  if (userId) {
+    return new Promise((resolve, reject) => {
+      const connection = dbContext.connect();
+      connection.on("connect", (err) => {
+        if (err) {
+          reject(err.message);
+        } else {
+          const request = new Request(
+            `INSERT INTO Codes (
+              id,
+              code
+              ) 
+              VALUES(
+                @id,
+                @code
+                )`,
+            (err) => {
+              if (err) {
+                reject(err.message);
+              }
+            }
+          );
+
+          let code = randomString.generate(20);
+          request.addParameter("id", TYPES.Int, userId);
+          request.addParameter("code", TYPES.VarChar, code);
+
+          try {
+            connection.execSql(request);
+            sendMail(email, code);
+
+            request.on("requestCompleted", function () {
+              resolve(email);
+            });
+          } catch (error) {
+            reject(error);
+          }
+        }
+      });
+      connection.connect();
+    });
+  }
+}
+
+function checkEmail(email) {
+  return new Promise((resolve, reject) => {
+    const connection = dbContext.connect();
+
+    connection.on("connect", (error) => {
+      if (error) {
+        reject(error.message);
+      } else {
+        const request = new Request(
+          `SELECT count (*) FROM Users WHERE email = @email`,
+          (err, rowCount) => {
+            if (err) {
+              reject(err.message);
+            }
+            if (rowCount != 0) {
+              reject("Account already exist!");
+            }
+          }
+        );
+
+        request.addParameter("email", TYPES.VarChar, email);
+
+        try {
+          connection.execSql(request);
+
+          request.on("row", (rows) => {
+            rows.forEach((row) => {
+              response = row.value;
+              resolve(response);
+            });
+          });
+        } catch (error) {
+          reject(error);
+        }
+      }
+    });
+    connection.connect();
+  });
+}
+
+function addUser(user) {
   return new Promise((resolve, reject) => {
     const connection = dbContext.connect();
 
@@ -33,16 +185,16 @@ function addUser(event) {
         );
         console.log("hello", request);
 
-        request.addParameter("firstName", TYPES.VarChar, event.firstName);
-        request.addParameter("lastName", TYPES.VarChar, event.lastName);
-        request.addParameter("email", TYPES.VarChar, event.email);
-        request.addParameter("password", TYPES.VarChar, event.password);
+        request.addParameter("firstName", TYPES.VarChar, user.firstName);
+        request.addParameter("lastName", TYPES.VarChar, user.lastName);
+        request.addParameter("email", TYPES.VarChar, user.email);
+        request.addParameter("password", TYPES.VarChar, user.password);
 
         try {
           connection.execSql(request);
 
           request.on("requestCompleted", function () {
-            resolve(event);
+            resolve(user);
           });
         } catch (error) {
           reject(error);
@@ -55,4 +207,7 @@ function addUser(event) {
 
 module.exports = {
   addUser,
+  checkEmail,
+  getUserId,
+  generateCode,
 };

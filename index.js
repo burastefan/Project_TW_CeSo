15c7ede95679ff230f59129be6813b1eee306fbc
Bura Stefan <stefan.bura@amplified.software>
2022-06-22 04:38:24 +0300

    test2

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 var http = require('http');
 let fs = require("fs");
+const path = require('path');
 
 http.createServer(function (req, res) {
-  fs.readFile("test.html",(err, data) => {
+  fs.readFile(path.resolve(__dirname, './test.html'),(err, data) => {
     if(!err) {
         res.writeHead(200, {'Content-Type': 'text/html'});
         res.end(data);
